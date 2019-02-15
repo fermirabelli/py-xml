72f52ade67e3ea9f32c632be4dd1581e5736726a
Fer <fermirabelli@hotmail.com>
2019-02-15 13:15:38 -0300

    prueba archivos 3

--- rest/test_write_file.py
+++ rest/test_write_file.py
@@ -1,13 +1,17 @@
 import uuid
 import datetime
 import hashlib
+import time
 
 def main():
     filename=uuid.uuid4().hex+'.txt'
     f= open(filename,"w+")
+    ts = time.time()
     for i in range(100):
-         text='<time>'+str(datetime.datetime.utcnow())+'</time>'+'<test>resultado suma :'+str(uuid.uuid4())+str( abs(hash(str(datetime.datetime.utcnow()))) % (10 ** 8))+' :  line %d\r\n' % (i+1)
-         f.write(text)
+        ts = time.time()
+        uid=uuid.uuid4()
+        text='<time>'+str(datetime.datetime.utcnow())+'</time>'+'<test>resultado suma :'+str(abs(hash(str(uid))) % (10 ** 8))+' + '+str( abs(hash(str(ts))) % (10 ** 8))+' :  line %d\r\n' % (i+1)+'</test><resultado>'+ str(abs(hash(str(uid))) % (10 ** 8)+abs(hash(str(ts))) % (10 ** 8))+'</resultado><elapsed>'+str(time.time()-ts)+'</elapsed><idtest>'+str(uuid.uuid4())+'</idtest>'
+        f.write(text)
     f.close() 
 
 if __name__=="__main__":
